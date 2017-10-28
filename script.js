06391943b33c74a6a2dd6c28314cce31669c056f
Nick Cerdan <ncerdan1229@gmail.com>
2017-10-27 21:08:35 -0700

    menu icons show info-panes on click

--- script.js
+++ script.js
@@ -4,18 +4,37 @@ function attachEventHandlers() {
 
 function handleImageClick(event) {
 	const target = $(event.target);
-	const menuImages = $('.menu-row .menu-image');
+	const menuImages = $('.menu-image');
+	const infoPanes = $('.info-pane');
 
 	if (target.hasClass('selected')) {
 		menuImages.removeClass('not-selected');
+		infoPanes.removeClass('selected');
 		target.removeClass('selected');
 	}
+
 	else {
 		menuImages.removeClass('selected');
 		menuImages.addClass('not-selected');
 
-		target.addClass('selected');
 		target.removeClass('not-selected');
+		target.addClass('selected');
+
+		infoPanes.removeClass('selected');
+
+		if (target.hasClass('home-image')) {
+			$('div.home-info-pane').addClass('selected');
+		} else if (target.hasClass('about-me-image')) {
+			$('div.about-me-info-pane').addClass('selected');
+		} else if (target.hasClass('education-image')) {
+			$('div.education-info-pane').addClass('selected');
+		} else if (target.hasClass('work-image')) {
+			$('div.work-info-pane').addClass('selected');
+		} else if (target.hasClass('projects-image')) {
+			$('div.projects-info-pane').addClass('selected');
+		} else {
+			$('div.contact-me-info-pane').addClass('selected');
+		}
 	}
 }
 
