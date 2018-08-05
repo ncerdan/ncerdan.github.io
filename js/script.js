3935d51fd79c63bab5917d7646cb75028f697365
Nick Cerdan <ncerdan1229@gmail.com>
2018-08-05 13:55:28 -0700

    first changes

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
+/*will handle all event*/
 function attachEventHandlers() {
 	$('.project').hover(handleProjectEnter, handleProjectLeave);
-	/*use?
-	$('.contact-item').hover(handleContactEnter, handleContactLeave);
-	*/
+	$('.contact-item').on('mouseenter', handleContactEnter);
 }
 
+/*moves title to top and brings description out at bottom*/
 function handleProjectEnter(event) {
 	$(event.currentTarget).addClass('hovered');
 	$(event.currentTarget).find('.project-title').animate({
@@ -13,6 +13,7 @@ function handleProjectEnter(event) {
 	$(event.currentTarget).find('.project-description').slideToggle(200);
 }
 
+/*returns title and description back to normal*/
 function handleProjectLeave(event) {
 	$(event.currentTarget).removeClass('hovered');
 	$(event.currentTarget).find('.project-title').animate({
@@ -21,12 +22,24 @@ function handleProjectLeave(event) {
 	$(event.currentTarget).find('.project-description').slideToggle(200);
 }
 
-/*
+/*make icons 'bounce'*/
 function handleContactEnter(event) {
+	$(event.currentTarget).find('.contact-icon').animate({
+		'bottom': '25px'
+	}, 100);
+	$(event.currentTarget).find('.contact-icon').animate({
+		'bottom': '0px'
+	}, 100);
+	$(event.currentTarget).find('.contact-icon').animate({
+		'bottom': '10px'
+	}, 65);
+	$(event.currentTarget).find('.contact-icon').animate({
+		'bottom': '0px'
+	}, 65);
 }
-*/
 
-$('document').ready(() => {
+/*readys event listeners*/
+$('document').ready(function() {
 	attachEventHandlers();
 });
 
