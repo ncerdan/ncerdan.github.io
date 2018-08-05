653f50f90e65095916bede2d45e3c489df772417
Nick Cerdan <ncerdan1229@gmail.com>
2018-08-04 19:38:55 -0700

    improved script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 function attachEventHandlers() {
-	$('.project').on('mouseenter', handleProjectHover);
-	$('.project').on('mouseleave', handleProjectLeave);
+	$('.project').hover(handleProjectEnter, handleProjectLeave);
+	/*use?
+	$('.contact-item').hover(handleContactEnter, handleContactLeave);
+	*/
 }
 
-function handleProjectHover(event) {
+function handleProjectEnter(event) {
 	$(event.currentTarget).addClass('hovered');
 	$(event.currentTarget).find('.project-title').animate({
 		'bottom': '90%'
@@ -17,8 +19,12 @@ function handleProjectLeave(event) {
 		'bottom': '0px'
 	}, 175);
 	$(event.currentTarget).find('.project-description').slideToggle(200);
-
 }
+
+/*
+function handleContactEnter(event) {
+}
+*/
 
 $('document').ready(() => {
 	attachEventHandlers();
